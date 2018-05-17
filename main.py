30128a0d430f494b42579edecc04ad8643c4177c
mingruimingrui <mingrui@visenze.com>
2018-05-17 09:12:57 +0800

    added main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
 import sys
 
+
 if __name__ == 'main':
     sys.path.insert(0, os.path.join(os.path.dirname(__file__name), '..'))
 
-
+from .data import load_dataset
 from . import config
+
+main()
+
+
+if __name__ == '__main__':
+    print('Start of test')
+    main()
+    print('End of test')
